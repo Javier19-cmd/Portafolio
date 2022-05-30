04264d8093a1449bb3727fe8800097677f1ea18e
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-05-29 18:56:26 -0600

    cambios

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./Cover.css"
-import coverVideo from "../../media/coverVideo.mp4"
+//import coverVideo from "../../media/coverVideo.mp4"
 
 const Cover = () => {
   return (
     <div className='cover-container'>
-        <video className="video" src={coverVideo} autoPlay loop muted />
+        <video className="video" src='./coverVideo.mp4' autoPlay loop muted />
         <h1>Javier Sebastián Valle Balsells</h1>
         <p>Programador de páginas web</p>
     </div>
